069b1551acbefbbd5b9bf3fa8b6ba48251860131
ashrafmohammad7 <ashrafmohammed07557@gmail.com>
2026-05-21 21:42:10 +0530

    Fixed Vercel Django deployment

--- api/index.py
+++ api/index.py
@@ -1,3 +1,11 @@
-from forexml.wsgi import application
+import os
+import sys
 
-app = application
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forexml.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
